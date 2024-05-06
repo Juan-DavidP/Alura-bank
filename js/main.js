432c855ca6c920f5178b5fa28acff13df6b31619
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2024-05-05 23:40:38 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import esUnCuil from "./validar-cuil.js";
+import esMayorDeEdad from "./validar-edad.js";
+
+const camposFormulario = document.querySelectorAll('[required]');
+
+camposFormulario.forEach((campo) => {
+  campo.addEventListener('blur', () => verificarCampo(campo));
+  campo.addEventListener('invalid', evento => evento.preventDefault())
+})
+
+function verificarCampo(campo) {
+  if (campo.name === 'cuil' && campo.value.length >= 11) {
+    esUnCuil(campo);
+  }
+
+  if (campo.name == 'fecha_nacimiento' && campo.value != "") {
+    esMayorDeEdad(campo);
+  }
+  console.log(campo.validity);
+}
